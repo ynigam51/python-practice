fc52096ff5319e4ea494e13c1ac7bf52c07aedcd
ynigam51 <81099531+ynigam51@users.noreply.github.com>
2021-03-25 17:42:35 +0530

    Add files via upload

---
+++ membership.y.py
@@ -0,0 +1,29 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=[2,4,5,6]
+>>> 2 in a
+True
+>>> 4 in a
+True
+>>>  5 in a
+ 
+SyntaxError: unexpected indent
+>>> 5 in a
+True
+>>> 6 in a
+True
+>>> 7 in a
+False
+>>> 2 not in a
+False
+>>> 4 not in a
+False
+>>> 5 not in a
+False
+>>> 6 not in a
+False
+>>> 7 not in a
+True
+>>> 9 not in a
+True
+>>> 
