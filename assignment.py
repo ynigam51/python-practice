3fd7683eaf68e72c10170d79786d6324e143a1b5
ynigam51 <81099531+ynigam51@users.noreply.github.com>
2021-03-25 17:37:07 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,27 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a,b=4,6
+>>> a=b
+>>> print(a)
+6
+>>> a+=b
+>>> print(b)
+6
+>>> print(a)
+12
+>>> a%=b
+>>> print(a)
+0
+>>> a//=b
+>>> print(a)
+0
+>>> a-=b
+>>> print(a)
+-6
+>>> a**=b
+>>> print(a)
+46656
+>>> a*=b
+>>> print(a)
+279936
+>>> 
