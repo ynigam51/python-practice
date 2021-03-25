104ae2b62b6df6f37dbdc0fe190b1291e2f4b3e2
ynigam51 <81099531+ynigam51@users.noreply.github.com>
2021-03-25 17:39:05 +0530

    Add files via upload

---
+++ bitwise.py
@@ -0,0 +1,14 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a,b=25,29
+>>> a & b
+25
+>>> a|b
+29
+>>> a^b
+4
+>>> ~a
+-26
+>>> ~b
+-30
+>>> 
