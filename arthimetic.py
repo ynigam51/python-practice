6d2d36b880b29f82d73c6f26f236f3c074064d4c
ynigam51 <81099531+ynigam51@users.noreply.github.com>
2021-03-25 17:23:06 +0530

    Add files via upload

---
+++ arthimetic.py
@@ -0,0 +1,20 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a,b=2,4
+>>> print(a+b)
+6
+>>> print(a-b)
+-2
+>>> print(a%b)
+2
+>>> print(a//b)
+0
+>>> print(a+=b)
+SyntaxError: invalid syntax
+>>> print(a/b)
+0.5
+>>> print(a**b)
+16
+>>> print(a*b)
+8
+>>> 
