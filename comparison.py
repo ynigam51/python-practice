36b3a64d28d71c12534b60e5433c051225d449cb
ynigam51 <81099531+ynigam51@users.noreply.github.com>
2021-03-25 17:25:21 +0530

    Add files via upload

---
+++ comparison.py
@@ -0,0 +1,16 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a,b=4,2
+>>> print(a==b)
+False
+>>> print(a!=b)
+True
+>>> print(a<=b)
+False
+>>> print(a>=b)
+True
+>>> print(a>b)
+True
+>>> print(a<b)
+False
+>>> 
